3892cc8d26061d12d6f43a9cdb82045480b7ede8
willschneider15 <willschneider15@gmail.com>
2023-02-16 01:43:54 -0500

    added record button to the UI

--- app/src/main/java/com/example/gptbros/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/gptbros/ui/home/HomeFragment.kt
@@ -5,9 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+//import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.gptbros.databinding.FragmentHomeBinding
+
 
 class HomeFragment : Fragment() {
 
@@ -22,17 +25,43 @@ class HomeFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
+
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
+
+//        val textView: TextView = binding.textHome
+////        val buttonText: TextView = binding.buttonRecord
+////        buttonText.text = "Record"
+////
+////        val button2 : Button = binding.buttonRecord
+////        button2.setOnClickListener{
+////            println("clicked button 2")
+////            Toast.makeText(view?.context, "Button Clicked", Toast.LENGTH_LONG).show()
+////        }
+//        homeViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//
+//        }
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+//        binding.textHome.text="This is an exceptionally hardcoded string"
+        val homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
         val textView: TextView = binding.textHome
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
+
         }
-        return root
+
+        binding.buttonRecord.setOnClickListener {
+            Toast.makeText(view.context, "New exercise will be generated", Toast.LENGTH_SHORT).show()
+            println("Generated a new exercise")
+
+        }
     }
 
     override fun onDestroyView() {
