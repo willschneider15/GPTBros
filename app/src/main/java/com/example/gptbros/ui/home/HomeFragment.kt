99e26c5e94c5e1fb380084d911b21aeb8a13d77f
Bouie782 <bouie.6@osu.edu>
2023-03-10 20:49:40 -0500

    Login activities and basic data calls

--- app/src/main/java/com/example/gptbros/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/gptbros/ui/home/HomeFragment.kt
@@ -1,7 +1,9 @@
 package com.example.gptbros.ui.home
 
+import android.content.ContentValues
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,7 +12,12 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.gptbros.MainActivity
 import com.example.gptbros.databinding.FragmentHomeBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 
 class HomeFragment : Fragment() {
@@ -20,6 +27,10 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    // Write a message to the database
+    val db = Firebase.firestore
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -50,6 +61,7 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 //        binding.textHome.text="This is an exceptionally hardcoded string"
+        auth = FirebaseAuth.getInstance()
         val homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
         val textView: TextView = binding.textHome
@@ -59,6 +71,7 @@ class HomeFragment : Fragment() {
         }
 
         binding.buttonRecord.setOnClickListener {
+            addSession()
             Toast.makeText(view.context, "New exercise will be generated", Toast.LENGTH_SHORT).show()
             println("Generated a new exercise")
 
@@ -66,6 +79,29 @@ class HomeFragment : Fragment() {
 
 //        binding.buttonRecord.setBackgroundColor(Color.RED)
     }
+
+    fun addSession(){
+        val audioSession = hashMapOf(
+            "sessionId" to 1,
+            "audiofile" to "file",
+            "transcription" to "trans_file",
+            "summary" to "sum_file",
+            "dateTimeStart" to "1/1/22-1:22",
+            "dateTimeEnd" to "1/1/22-1:56",
+            "audiofileURL" to "temp_url"
+        )
+
+        // Add a new document with a generated ID
+        db.collection("users/"+auth.currentUser?.email.toString())
+            .add(audioSession)
+            .addOnSuccessListener { documentReference ->
+                Log.d(ContentValues.TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(ContentValues.TAG, "Error adding document", e)
+            }
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
