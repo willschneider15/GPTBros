730e4b3734b42562bbaf395c1b0aac456c8ec98e
lukeshotgun <wingert.18@buckeyemail.osu.edu>
2023-04-01 14:44:38 -0400

    added adapterId

--- app/src/main/java/com/example/gptbros/ui/folder/FolderFragment.kt
+++ app/src/main/java/com/example/gptbros/ui/folder/FolderFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -12,17 +13,23 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.gptbros.databinding.FragmentFolderBinding
+import com.example.gptbros.model.FolderListItem
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class FolderFragment : Fragment() {
 
     private var _binding: FragmentFolderBinding? = null
+
     private val folderViewModel : FolderViewModel by viewModels()
     // This property is only valid between onCreateView and
     // onDestroyView.
-    private val binding get() = _binding!!
+    private val binding
+        get() = checkNotNull(_binding) {
+            "Cannot access binding because it is null. Is the view visible?"
+        }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -33,6 +40,7 @@ class FolderFragment : Fragment() {
     ): View {
 
         _binding = FragmentFolderBinding.inflate(inflater, container, false)
+        binding.folderRecyclerView.layoutManager = LinearLayoutManager(context)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
@@ -49,7 +57,7 @@ class FolderFragment : Fragment() {
         viewLifecycleOwner.lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 folderViewModel.folderListItems.collect() {items ->
-                    //_binding.folderRecyclerView.adapter = FolderListAdapter(items)
+                    //binding.folderRecyclerView.adapter = FolderListItemAdapter(items)
                 }
             }
         }
