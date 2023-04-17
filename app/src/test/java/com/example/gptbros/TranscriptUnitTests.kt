1d2e4892f8438adb3da80c45d5b0f2af1f638fc9
Bouie782 <bouie.6@osu.edu>
2023-04-17 19:19:08 -0400

    Transcript api tests complete

--- app/src/test/java/com/example/gptbros/TranscriptUnitTests.kt
+++ app/src/test/java/com/example/gptbros/TranscriptUnitTests.kt
@@ -1,11 +1,18 @@
 package com.example.gptbros
 
+import android.content.Context
+import android.os.Build.VERSION_CODES.Q
 import android.os.Environment
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.gptbros.model.GptBrosRepository
+import com.example.gptbros.ui.MainActivity
 import com.example.gptbros.ui.home.HomeFragment
 import com.example.gptbros.ui.home.HomeViewModel
 import io.ktor.util.reflect.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.newCoroutineContext
 import kotlinx.coroutines.newSingleThreadContext
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
@@ -14,14 +21,21 @@ import org.junit.After
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
 
+@RunWith(AndroidJUnit4::class)
+@Config(sdk = [Q])
 class TranscriptUnitTests {
 
     private val mainThreadSurrogate = newSingleThreadContext("UI thread")
+    private val context = ApplicationProvider.getApplicationContext<Context>()
+    private val hvm = HomeViewModel()
 
     @Before
     fun setUp() {
         Dispatchers.setMain(mainThreadSurrogate)
+        GptBrosRepository.initialize(context)
     }
 
     @After
@@ -30,17 +44,17 @@ class TranscriptUnitTests {
         mainThreadSurrogate.close()
     }
 
+    //Change these vars when testing audio files.
     val testRecording1 = "This is a test"
-    val testFP1 = "test1.wav"
+    val testFP1 = "sampledata/test1.wav"
     val testRecording2 = "Hi I am Anthony, this is a test of the app"
-    val testFP2 = "test2.wav"
+    val testFP2 = "sampledata/test2.wav"
+
 
 
     private suspend fun getTS(rec:String): String {
-        var hvm = HomeViewModel()
-        var context = HomeFragment().context
-        val filePath : String = Environment.getExternalStorageDirectory().absolutePath + "/"+ rec
-        return context?.let { hvm.transcribeRecording(it, filePath) }!!
+        val filePath : String = rec
+        return hvm.transcribeRecording(context, filePath)
     }
 
 
